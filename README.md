06f5d010b7eea10c1d6092b76e04d3e4371a4228
ShubhamNagure <nagure.shubham@gmail.com>
2020-10-02 02:39:32 +0530

    Added OOP approach to project

--- README.md
+++ README.md
@@ -12,4 +12,15 @@ This  is an interactive game. You will have to enter choices to navigate the gam
 3. A right choice will take you closer to winning and a wrong choice will take you closer to loosing.
 4. The objective of the game is to win by responding correctly to all the tasks.
 
+
+##Project Structure
+
+Here I used Object oriented approach. Its just a fancy way to represent the code which improves code readability and let devs work more on different modules independently.
+1. Info.py - will help you to get players info.
+2. Game.py - will describe the rules of the game.
+3. dungeon.py - Is main file which drives the whole process.
+
+Now whole process is well organized to develope further features. ;)
+
 Enjoy!
+
--- dungeon.py
+++ dungeon.py
@@ -1,53 +1,22 @@
+from info import PlayerInfo as info
+from Game import Game as game
+
 print("DUNGEON GAME! Make your choices, well")
+class Flow:
 
-name= input("Hey! What's yout name? ")
-age = int(input("How old are you? "))
+    def main(self):
+        """
+        Main Method will drive all operations.
+        :return:
+        Fun ;)
+        """
+        _age= info.getInfo(self)
+        _flag = game.check_age(_age)
+        if _flag == False:exit()
+        game.wantsToPlay(0)
 
-print("Namaste", name, "so you're", age, "huh!")
-
-health = 20
-
-print("You are starting with", health, "health")
-
-if age>=16:
-    print("You're old enough to play!")
-
-    wants2play = input("Do you want to play?(yes/no) ").lower()
-    if wants2play == "yes":
-        print("Let's PLAY!")
-        lor = input("First choice... Left or Right (left/right)?").lower()
-        if lor =="left":
-            ans = input("Alright! Now you followed the path, exhausted, you walk and reach out to a lake. You quench your thirst. Do you swim across or go around (across, around)?").lower()
-
-            if ans == "around":
-                print("You are a wise person choosing a looooooooong way to reach his destination. Hardwork always pays off. You went around and reached the other side of the lake.")
-            elif ans == "across":
-                print("Swimmer! Unfortunately, you were caught into a deadly battle with a crocodile! Your health is halved.")
-                health -= 10
-
-            ans = input("You notice a house and a river. Which do you go to (river/house)? ")
-            if ans == "house":
-                print("You go to the house and are greeted by a beautiful mistress... She doesn't like the way you behave and you lose 5 health")
-                health -= 10
-
-                if health <= 0:
-                    print("You now have 0 health and you lost the game...")
-                else:
-                    print("You have survived ... You win!")
-            if ans == "river":
-                ride = input("Would you like to take boat ride in river (yes/no)? ")
-                if ride == "yes":
-                    print("Ohhh water entered into boat!")
-                    print("Alas!!!  You Lost!")
-                else:
-                    print("Mad you can't cross river without boat, You lost!!!")
-            else:
-                print("You lost dummy. This is not even a choiceee!!")
-        else:
-            print("You fell down into a well, blind person! You lost")
-
-    else:
-        print("Well, see ya soon!")
-    
-else:
-    print("You're not old enough. SORRY :(")
+"""
+Here object created and and called main method
+"""
+ObjFlow = Flow()
+ObjFlow.main()
