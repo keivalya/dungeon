fe907c07bd20c4416b5cef27181f2a215ebe96ef
Brandon Chung <delta@Brandons-MacBook-Pro.local>
2020-10-01 21:48:24 -0500

    Added inventory and interaction at river

--- dungeon.py
+++ dungeon.py
@@ -12,6 +12,8 @@ print("Namaste", name, "so you're", age, "huh!")
 
 health = 20
 
+inventory = []
+
 if age >= 16:
     print("You're old enough to play!")
 
@@ -27,12 +29,10 @@ if age >= 16:
             if ans == "around":
                 print("You are a wise person choosing a looooooooong way to reach his destination. Hardwork always pays off. You went around and reached the other side of the lake.")
             elif ans == "across":
-                print(
-                    "Swimmer! Unfortunately, you were caught into a deadly battle with a crocodile! Your health is halved.")
+                print("Swimmer! Unfortunately, you were caught into a deadly battle with a crocodile! Your health is halved.")
                 health -= 10
 
-            ans = input(
-                "You notice a house and a river. Which do you go to (river/house)? ")
+            ans = input("You notice a house and a river. Which do you go to (river/house)? ")
             if ans == "house":
                 print("You go to the house and are greeted by a beautiful mistress... She doesn't like the way you behave and you lose 5 health")
                 health -= 10
@@ -42,8 +42,40 @@ if age >= 16:
                 else:
                     print("You have survived ... You win!")
 
+            elif ans == "river":
+                print("You walk to the river and discover a short man eating alone by a fire.")
+                greet_or_fight = input("Do you greet him or try to fight him for his food? (greet/fight)").lower()
+                if greet_or_fight == "greet":
+                    print("After taking a moment to look at you, the man decides that you are of no harm")
+                    print("He decides to share some of his food with you.")
+                    if health < 20:
+                        print("You enjoy a nice conversation with eating. You feel better after the delicious meal.")
+                        # TODO Decide if health should be cappedat 20. If not, just heal the specified amount
+                        if health > 15:
+                            health = 20
+                        else:
+                            health += 5
+                    else:
+                        print("You don't feel healthier, but you enjoyed a nice conversation while eating the delicious meal.")
+                elif greet_or_fight == "fight":
+                    print("You charge at the man and tackle him down to the ground. He takes out a knife and slashes you near your gut.")
+                    health -= 15
+                    if health <= 0:
+                        print("You died over a meal that you didn't even get to taste. Was it worth it?")
+                    else:
+                        print("You survive the first slash and manage wrestle the knife out of his hand.")
+                        print("You leap for the knife and threaten to harm your opponent if he doesn't give you his entire meal. He yields and allows you toleave with his knife and the meal.")
+                        print("You recover a large amount of health after eating the entire meal.")
+                        # TODO Change if health shouldn't be capped at 20
+                        if health < 3:
+                            health += 17
+                        else:
+                            health = 20
+                        print("You now have a knife in your inventory")
+                        inventory.append("knife")
+                        print("You survived so far, but are you proud of what you have done?")
             else:
-                print("You lost dummy. This is not even a choiceee!!")
+                print("You couldn't make up you mind, so you stood still until you starved to death")
         else:
             print("You fell down into a well, blind person! You lost")
 
